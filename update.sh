d0176212b5fa3980676defce817eff5288302dcf
tools4cydia <72500625+tools4cydia@users.noreply.github.com>
2024-01-12 20:00:06 +0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+cd $(dirname "$0")
+
+dpkg-scanpackages -m ./debfiles > Packages
+bzip2 -c Packages > Packages.bz2
+gzip -c Packages > Packages.gz
+
+
 dpkg-scanpackages -m ./debfiles > Packages
 bzip2 -c Packages > Packages.bz2
 gzip -c Packages > Packages.gz
