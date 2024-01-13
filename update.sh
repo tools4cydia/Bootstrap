16f2e3910ea3f9664e0ad5de307bf61c8b0a5448
tools4cydia <72500625+tools4cydia@users.noreply.github.com>
2024-01-13 20:17:21 +0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,17 +3,3 @@ cd $(dirname "$0")
 
 dpkg-scanpackages -m ./debfiles > Packages
 bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
-
-
-dpkg-scanpackages -m ./debfiles > Packages
-bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
-
-cd beta
-
-dpkg-scanpackages -m ./debfiles > Packages
-bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
-
-cd ..
